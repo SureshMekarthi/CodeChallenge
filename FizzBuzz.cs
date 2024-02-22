a7f993eabdb42f497a45bb87af2b8470696fb46d
AnushaPoosala <anusha.poosala@outlook.com>
2024-02-21 21:28:08 -0500

    FizzBuzz

---
+++ FizzBuzz.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CodeChallenge
+{
+    internal class FizzBuzz
+    {
+        static void Main(string[] args)
+        {
+            int input = 100;
+            FizzBuzzprint(input);
+            Console.ReadLine();
+        }
+
+        private static void FizzBuzzprint(int input)
+        {
+            for (int i = 1; i <= input; i++)
+            {
+                if (i % 3 == 0 && i % 5 == 0)
+                {
+                    Console.WriteLine("FizzBuzz");
+                }
+                else if (i % 3 == 0)
+                {
+                    Console.WriteLine("Fizz");
+                }
+                else if (i % 5 == 0)
+                {
+                    Console.WriteLine("Buzz");
+                }
+                else
+                {
+                    Console.WriteLine(i);
+                }
+            }
+        }
+    }
+}
+}
